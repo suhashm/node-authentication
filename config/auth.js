19c18e501eb5fd62d7698d59b64d352bae44fb12
suhashm <mandahasa2626@gmail.com>
2014-04-03 02:27:03 +0530

    Corrected FB Call back URL to herokuapp

--- config/auth.js
+++ config/auth.js
@@ -5,19 +5,22 @@ module.exports = {
     'facebookAuth' : {
         'clientID' 		: '1573165476241371', // your App ID
         'clientSecret' 	: 'cae8aca7858070bf8365dc6e039987e9', // your App Secret
-        'callbackURL' 	: 'http://localhost:8080/auth/facebook/callback'
+        'callbackURL' 	: 'http://floating-island-1816.herokuapp.com/auth/facebook/callback'
+        //'callbackURL' 	: 'http://localhost:8080/auth/facebook/callback'
     },
 
     'twitterAuth' : {
         'consumerKey' 		: 'your-consumer-key-here',
         'consumerSecret' 	: 'your-client-secret-here',
         'callbackURL' 		: 'http://localhost:8080/auth/twitter/callback'
+        //'callbackURL' 		: 'http://floating-island-1816.herokuapp.com/auth/twitter/callback'
     },
 
     'googleAuth' : {
         'clientID' 		: 'your-secret-clientID-here',
         'clientSecret' 	: 'your-client-secret-here',
         'callbackURL' 	: 'http://localhost:8080/auth/google/callback'
+        //'callbackURL' 	: 'http://floating-island-1816.herokuapp.com/auth/google/callback'
     }
 
 };
