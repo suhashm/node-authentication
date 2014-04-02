5a86a2f4f039ff429643765acc28e5bd7967b414
suhashm <mandahasa2626@gmail.com>
2014-04-03 03:03:15 +0530

    Implemented Facebook Authentication using secret id and key

--- README.md
+++ README.md
@@ -3,5 +3,10 @@ node-authentication
 
 Node Authentication using PassportJS (Local, Facebook, Twitter and Google+ Strategies)
 
-Added Facebook Authentication
+<b>Installation</b>
+<ol>
+    <li>npm install</li>
+    <li>For Facebook Authentication: set client id and secret key in auth.js</li>
+</ol>
+
 <a href="http://floating-island-1816.herokuapp.com/"><b>Demo</b></a>
--- config/auth.js
+++ config/auth.js
@@ -3,8 +3,8 @@
 module.exports = {
 
     'facebookAuth' : {
-        'clientID' 		: '1573165476241371', // your App ID
-        'clientSecret' 	: 'cae8aca7858070bf8365dc6e039987e9', // your App Secret
+        'clientID' 		: 'your-consumer-key-here', // your App ID
+        'clientSecret' 	: 'your-client-secret-here', // your App Secret
         'callbackURL' 	: 'http://floating-island-1816.herokuapp.com/auth/facebook/callback'
         //'callbackURL' 	: 'http://localhost:8080/auth/facebook/callback'
     },
